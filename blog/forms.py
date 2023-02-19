095f89fcc43a7f1fb45bfc97df2cb38169f60f63
Muzzius <murraybosworth@gmail.com>
2023-02-19 12:23:02 +0000

    Added docstrings to blogs/forms.py

--- blog/forms.py
+++ blog/forms.py
@@ -3,6 +3,9 @@ from django import forms
 
 
 class CommentForm(forms.ModelForm):
+    """
+    Creates a new comment form object to be called by the PostDetail view
+    """
     class Meta:
         model = Comment
         fields = ['body']
