60e45bc100021af5eb890e52df6260698f3ee225
Muzzius <murraybosworth@gmail.com>
2023-02-19 12:22:07 +0000

    Added docstrings to blogs/views.py

--- blog/views.py
+++ blog/views.py
@@ -1,13 +1,22 @@
 from django.views.generic import DetailView
-from .models import Post, Comment
+from .models import Comment
 from .forms import CommentForm
 
 
 # Create your views here.
 class PostDetail(DetailView):
-    model = Post
-
+    """
+    Creates an object as a subclass of DetailView to display posts on the website.
+    """
     def get_context_data(self, **kwargs):
+        """
+        This method pulls the data for a post object to display it on the site.
+        storing it in a dictionary
+
+        :return: Returns the context data for a post including attached comments if there are any.
+        If the user is authenticated it will also include the commentForm to allow them to leave
+        a comment.
+        """
         data = super().get_context_data(**kwargs)
 
         comments_connected = Comment.objects.filter(
@@ -18,6 +27,12 @@ class PostDetail(DetailView):
         return data
 
     def post(self, request, *args, **kwargs):
+        """
+        This method receives the context data from the CommentForm and post it
+        from within the class based PostDetail view
+
+        :return: POSTs the new comment data to our server to be saved in our database
+        """
         new_comment = Comment(body=request.POST.get('body'),
                               author=self.request.user,
                               blog_post=self.get_object())
